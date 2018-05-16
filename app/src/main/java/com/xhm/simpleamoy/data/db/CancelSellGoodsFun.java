1f504095dead71115574d54fb22d81161c236ec4
ASUS <747586234@qq.com>
2018-05-16 12:16:41 +0800

    卖出取消

--- app/src/main/java/com/xhm/simpleamoy/data/db/CancelSellGoodsFun.java
+++ app/src/main/java/com/xhm/simpleamoy/data/db/CancelSellGoodsFun.java
@@ -1,5 +1,7 @@
 package com.xhm.simpleamoy.data.db;
 
+import android.util.Log;
+
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
@@ -23,7 +25,52 @@ public abstract class CancelSellGoodsFun {
     }
 
     public void cancelSellOneGoods(){
+        Log.i("cancelSellOneGoods: ", String.valueOf(mSellGoods.size()));
+        ArrayList arrayList=new ArrayList();
+        for(int i=0;i<mSellGoods.size();i++)
+        {
+            String id=mSellGoods.get(i).getSellUUID();
+            arrayList.add(id);
 
+        }
+        AVQuery avQuery = new AVQuery("Products");
+        avQuery.whereContainedIn("goodsUUID",arrayList);
+        avQuery.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, AVException e) {
+
+                if (e==null)
+                {
+                    if (!list.isEmpty()) {
+                        for (int i=0;i<list.size();i++) {
+                            AVObject avObject = (AVObject) list.get(i);
+                            if (avObject.getBoolean("isByBuy")) {
+                                AVObject todo = AVObject.createWithoutData("Products", ((AVObject) list.get(i)).getObjectId());
+                                todo.put("isByBuy", false);
+                                todo.put("buyUserName", null);
+                                todo.saveInBackground();
+
+                            } else cancelSellGoodsFaild("错误！");
+
+
+
+
+
+                        }
+                        cancelSellGoodsSucess();
+                    }
+                    else cancelSellGoodsFaild("!!!!!!！");
+
+
+                }
+                else cancelSellGoodsFaild(e.getMessage());
+
+
+
+            }
+
+
+        });
 
 
 
