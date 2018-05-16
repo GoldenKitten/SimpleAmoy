b33bd95fb1fbe5a16a67918ae2d8c1b3a3d5ce07
ASUS <747586234@qq.com>
2018-05-16 22:02:42 +0800

    头像修改

--- app/src/main/java/com/xhm/simpleamoy/data/db/ModifyHeadImage.java
+++ app/src/main/java/com/xhm/simpleamoy/data/db/ModifyHeadImage.java
@@ -1,5 +1,13 @@
 package com.xhm.simpleamoy.data.db;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.FindCallback;
+
+import java.util.List;
+
 /**
  * Created by xhm on 2018/5/16.
  */
@@ -14,6 +22,28 @@ public abstract class ModifyHeadImage {
     }
 
     private void modifyHeadImage() {
+        AVQuery avQuery=new AVQuery("RegistUser");
+        avQuery.whereEqualTo("userName",mUserName);
+        avQuery.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, AVException e) {
+
+                if (e==null)
+                {
+                    if (!list.isEmpty())
+                    {AVObject avObject=AVObject.createWithoutData("RegistUser",((AVObject) list.get(0)).getObjectId());
+
+                    avObject.put("headImage",new AVFile(mUserName+ ".RegistUserPic",mHeadImage));
+                    avObject.saveInBackground();
+                    modifySucess();
+                }
+                }
+                else modifyFailed(e.getMessage());
+
+            }
+
+
+        });
     }
     public abstract void modifySucess();
     public abstract void modifyFailed(String msg);
