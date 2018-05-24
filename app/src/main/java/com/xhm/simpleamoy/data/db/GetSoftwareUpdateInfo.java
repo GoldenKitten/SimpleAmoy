72b0d5d20295d3f7e0831feb6b8e31947daba79f
白日梦想家 <747586234@qq.com>
2018-05-24 18:22:28 +0800

    更新 GetSoftwareUpdateInfo.java

--- app/src/main/java/com/xhm/simpleamoy/data/db/GetSoftwareUpdateInfo.java
+++ app/src/main/java/com/xhm/simpleamoy/data/db/GetSoftwareUpdateInfo.java
@@ -12,6 +12,35 @@ public abstract class GetSoftwareUpdateInfo {
     }
 
     private void getSoftwareUpdateInfo() {
+        AVQuery avQuery=new AVQuery("apk");
+        avQuery.findInBackground(new FindCallback() {
+            @Override
+            public void done(List list, AVException e) {
+       if (e==null)
+       {
+           if (list.isEmpty())
+           {
+               AVObject avObject= (AVObject) list.get(0);
+               String versionName= avObject.getString("versionName");
+               int versionCode= (int) avObject.getNumber("versionCode");
+               AVFile avFile=avObject.getAVFile("url");
+               String url=avFile.getUrl();
+               AppInfo appInfo=new AppInfo();
+               appInfo.setUrl(url);
+               appInfo.setVersionCode(versionCode);
+               appInfo.setVersionName(versionName);
+               getSoftwareUpdateSucess(appInfo);
+
+           }
+           else getSoftwareUpdateFailed("未查到数据");
+
+
+       }
+       else getSoftwareUpdateFailed(e.getMessage());
+            }
+
+
+        });
     }
     public abstract void getSoftwareUpdateSucess(AppInfo appInfo);
     public abstract void getSoftwareUpdateFailed(String msg);
